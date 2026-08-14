9a3d1b15a180d7901635c6fea9b5590838687522
Ritik Rawat <ritikrawat0710@gmail.com>
2026-08-14 19:43:34 +0530

    chore(api): Add `UsersService`

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [],
-  exports: [],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
